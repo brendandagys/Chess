2eac3fb8a41ebe1018bf78c8a0b65f464b8e8377
Brendan Dagys <brendandagys@gmail.com>
2025-03-19 18:03:01 -0400

    Significant updates to `connect` handler function, including addition of tracing

--- sam/rust_app/src/connect.rs
+++ sam/rust_app/src/connect.rs
@@ -2,31 +2,36 @@ mod helpers;
 mod types;
 mod utils;
 
+use aws_lambda_events::apigw::ApiGatewayProxyResponse;
 use helpers::game::{assign_player_to_remaining_slot, create_game, get_game, save_game};
-use helpers::user::{create_user_game, get_user_game};
+use helpers::user::{create_user_game, get_user_game, save_user_record};
+use types::pieces::Color;
+use utils::api_gateway::post_to_connection;
 
 use aws_config::BehaviorVersion;
+use aws_sdk_apigatewaymanagement as apigw;
 use aws_sdk_dynamodb::Client;
 use lambda_http::aws_lambda_events::apigw::ApiGatewayWebsocketProxyRequest;
 use lambda_http::LambdaEvent;
 use lambda_runtime::{run, service_fn, Error};
-use std::env;
-use types::lambda_runtime::CustomResponse;
-use types::pieces::Color;
 
 async fn function_handler(
     event: LambdaEvent<ApiGatewayWebsocketProxyRequest>,
-    client: &Client,
-) -> Result<CustomResponse, Error> {
+    dynamo_db_client: &Client,
+    api_gateway_client: &apigw::Client,
+) -> Result<ApiGatewayProxyResponse, Error> {
+    let game_table = std::env::var("GAME_TABLE").unwrap();
+    let user_table = std::env::var("USER_TABLE").unwrap();
+
     let request_context = event.payload.request_context;
     let query_params = event.payload.query_string_parameters;
 
-    // Mandatory
+    // Mandatory query parameters
     let username = query_params
         .first("username")
         .ok_or_else(|| Error::from("Username is required"))?;
 
-    // Optional
+    // Optional query parameters
     let game_id = query_params.first("game_id");
     let color_preference = query_params
         .first("color")
@@ -37,45 +42,88 @@ async fn function_handler(
         .connection_id
         .ok_or_else(|| Error::from("Missing connection ID"))?;
 
-    // Initialize DynamoDB client
-    let game_table = env::var("GAME_TABLE").unwrap();
-    let user_table = env::var("USER_TABLE").unwrap();
-
     let game = match game_id {
-        Some(game_id) => match get_game(&client, &game_table, game_id).await {
-            Ok(game) => assign_player_to_remaining_slot(game, username, &connection_id)?,
-            Err(_) => create_game(Some(game_id), username, color_preference, &connection_id),
+        Some(game_id) => match get_game(&dynamo_db_client, &game_table, game_id).await {
+            Ok(found_game) => {
+                tracing::info!(
+                    "Found existing game for {username} (ID: {})",
+                    found_game.game_id
+                );
+                assign_player_to_remaining_slot(found_game, username, &connection_id)?
+            }
+            Err(_) => {
+                let new_game =
+                    create_game(Some(game_id), username, color_preference, &connection_id);
+                tracing::info!("Created new game for {username} (ID: {})", new_game.game_id);
+                new_game
+            }
         },
-        None => create_game(None, username, color_preference, &connection_id),
+        None => {
+            let new_game = create_game(None, username, color_preference, &connection_id);
+            tracing::info!("Created new game for {username} (ID: {})", new_game.game_id);
+            new_game
+        }
     };
 
-    save_game(&client, &game_table, &game).await?;
+    save_game(&dynamo_db_client, &game_table, &game).await?;
 
-    if let Err(_) = get_user_game(&client, &user_table, username, &game.game_id).await {
-        create_user_game(&game.game_id, username);
+    // Retrieve or create a new user-game record and assign user's connection ID to it
+    match get_user_game(&dynamo_db_client, &user_table, username, &game.game_id).await {
+        Ok(mut found_user_game) => {
+            found_user_game.connection_id = Some(connection_id.clone());
+            tracing::info!(
+                "Found existing user-game record for {username} (ID: {})",
+                found_user_game.sort_key
+            );
+            save_user_record(dynamo_db_client, &user_table, &found_user_game).await?;
+        }
+        Err(_) => {
+            let new_user_game = create_user_game(&game.game_id, username, &connection_id);
+            save_user_record(dynamo_db_client, &user_table, &new_user_game).await?;
+            tracing::info!(
+                "Created new user-game record for {username} (ID: {})",
+                new_user_game.sort_key
+            );
+        }
+    };
+
+    // Update connected players
+    if let Some(white_connection_id) = &game.white_connection_id {
+        post_to_connection(api_gateway_client, white_connection_id, &game).await?;
+        tracing::info!("Sent game (ID: {}) update to white player", game.game_id);
     }
 
-    Ok(CustomResponse {
+    if let Some(black_connection_id) = &game.black_connection_id {
+        post_to_connection(api_gateway_client, black_connection_id, &game).await?;
+        tracing::info!("Sent game (ID: {}) update to black player", game.game_id);
+    }
+
+    tracing::info!("USER {username} CONNECTED TO GAME (ID: {})", game.game_id);
+
+    Ok(ApiGatewayProxyResponse {
         status_code: 200,
-        body: format!("{username} joined game (ID: {}) successfully", game.game_id),
+        body: Some((format!("{username} joined game (ID: {})", game.game_id)).into()),
+        ..Default::default()
     })
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let client: Client = Client::new(&aws_config::load_defaults(BehaviorVersion::latest()).await);
+    let sdk_config = aws_config::load_defaults(BehaviorVersion::latest()).await;
+    let dynamo_db_client: Client = Client::new(&sdk_config);
+    let api_gateway_client: apigw::Client = apigw::Client::new(&sdk_config);
 
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
-        // disable printing the name of the module in every log line.
-        .with_target(false)
-        // disabling time is handy because CloudWatch will add the ingestion time.
+        // Include the name of the module in every log line
+        .with_target(true)
+        // CloudWatch will add the ingestion time
         .without_time()
         .init();
 
     run(service_fn(
         |event: LambdaEvent<ApiGatewayWebsocketProxyRequest>| async {
-            function_handler(event, &client).await
+            function_handler(event, &dynamo_db_client, &api_gateway_client).await
         },
     ))
     .await?;
