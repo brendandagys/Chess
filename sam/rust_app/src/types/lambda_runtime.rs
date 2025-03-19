2d4db65afc78222e1c8a35c6bd8af2c05d834827
Brendan Dagys <brendandagys@gmail.com>
2025-03-19 18:00:45 -0400

    Remove own `lambda_runtime` types module

--- sam/rust_app/src/types/lambda_runtime.rs
+++
@@ -1,8 +0,0 @@
-use serde::Serialize;
-
-#[derive(Serialize)]
-pub struct CustomResponse {
-    #[serde(rename = "status_code")]
-    pub status_code: i32,
-    pub body: String,
-}
--- sam/rust_app/src/types/mod.rs
+++ sam/rust_app/src/types/mod.rs
@@ -1,5 +1,4 @@
 pub mod board;
 pub mod dynamo_db;
 pub mod game;
-pub mod lambda_runtime;
 pub mod pieces;
