5c65d78b0412aa1380e8ed926e3994a970f5788f
Brendan Dagys <brendandagys@gmail.com>
2025-03-27 18:14:08 -0400

    Add game-level toast messages

--- frontend/src/components/Game.tsx
+++ frontend/src/components/Game.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-base-to-string */
+import { useState, useEffect } from "react";
 import { GameRecord } from "../types/game";
 import { Color } from "../types/piece";
 import { ChessBoard } from "./ChessBoard";
@@ -11,6 +12,11 @@ interface GameProps {
 }
 
 export const Game: React.FC<GameProps> = ({ gameRecord, usernames }) => {
+  const [alertMessage, setAlertMessage] = useState<string | null>(null);
+  const [alertColor, setAlertColor] = useState<"green" | "blue" | "red">(
+    "blue"
+  );
+
   const gameState = gameRecord.game_state;
 
   const playerColor = usernames.includes(gameRecord.white_username ?? "")
@@ -19,19 +25,57 @@ export const Game: React.FC<GameProps> = ({ gameRecord, usernames }) => {
 
   const isTurn = playerColor === gameState.currentTurn;
 
+  useEffect(() => {
+    if (alertMessage) {
+      const timer = setTimeout(() => {
+        setAlertMessage(null);
+      }, 5000);
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [alertMessage]);
+
+  const dismissAlert = () => {
+    setAlertMessage(null);
+  };
+
   return (
     <div className="game-container">
-      <h2>Game: {gameRecord.game_id}</h2>
-      <div className="status-container">
-        <p className="pill pill--green">
-          Game {gameState.state.toString().replace("-", " ")}
-        </p>
-        <p className="pill pill--blue">
-          {gameState.currentTurn.toString().replace("w", "W").replace("b", "B")}
-          {"'s turn"}
-        </p>
-        <p className="pill pill--pink">Playing as {playerColor}</p>
-      </div>
+      <h2 className="game-id">Game: {gameRecord.game_id}</h2>
+
+      {alertMessage ? (
+        <div
+          className={`alert alert--${alertColor} ${
+            !alertMessage ? "hidden" : ""
+          }`}
+        >
+          <span className="message">{alertMessage}</span>
+          <button className="alert-dismiss" onClick={dismissAlert}>
+            &times;
+          </button>
+        </div>
+      ) : (
+        <div className="status-container">
+          <p
+            className="pill pill--green"
+            onClick={() => {
+              setAlertMessage("Game started!");
+              setAlertColor("green");
+            }}
+          >
+            Game {gameState.state.toString().replace("-", " ")}
+          </p>
+          <p className="pill pill--blue">
+            {gameState.currentTurn
+              .toString()
+              .replace("w", "W")
+              .replace("b", "B")}
+            {"'s turn"}
+          </p>
+          <p className="pill pill--pink">Playing as {playerColor}</p>
+        </div>
+      )}
 
       <div className={`chess-board-container ${isTurn && "is-player-turn"}`}>
         <ChessBoard board={gameState.board} playerColor={playerColor} />
