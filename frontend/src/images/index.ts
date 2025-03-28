67d9c979f28e722948f948d16dd0c61bfdaefcec
Brendan Dagys <brendandagys@gmail.com>
2025-03-28 16:55:36 -0400

    Re-export images to reduce import clutter

---
+++ frontend/src/images/index.ts
@@ -0,0 +1,27 @@
+import wp from "../images/wp.png";
+import bp from "../images/bp.png";
+import wr from "../images/wr.png";
+import br from "../images/br.png";
+import wn from "../images/wn.png";
+import bn from "../images/bn.png";
+import wb from "../images/wb.png";
+import bb from "../images/bb.png";
+import wq from "../images/wq.png";
+import bq from "../images/bq.png";
+import wk from "../images/wk.png";
+import bk from "../images/bk.png";
+
+export const images = {
+  wp,
+  bp,
+  wr,
+  br,
+  wn,
+  bn,
+  wb,
+  bb,
+  wq,
+  bq,
+  wk,
+  bk,
+};
