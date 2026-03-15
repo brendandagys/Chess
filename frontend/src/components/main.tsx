c419585182b80574236f0be8ab6ecf05959eda25
Brendan Dagys <brendandagys@gmail.com>
2026-03-15 19:53:51 -0400

    Re-enable StrictMode

--- frontend/src/components/main.tsx
+++ frontend/src/components/main.tsx
@@ -1,3 +1,4 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { App } from "@src/components/App.tsx";
@@ -7,7 +8,9 @@ import "@src/css/index.css";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
-  <NavProvider>
-    <App />
-  </NavProvider>
+  <StrictMode>
+    <NavProvider>
+      <App />
+    </NavProvider>
+  </StrictMode>,
 );
